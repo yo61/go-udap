c27b6cf265ea7630a2a9bd16dc883d04d1f2d8e2
Robin Bowes <robin@yo61.com>
2026-05-08 10:26:52 +0100

    feat(cli): implement save, reset, and commit subcommands

---
+++ cli/commit.go
@@ -0,0 +1,47 @@
+package cli
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"time"
+
+	"github.com/spf13/pflag"
+)
+
+func runCommit(args []string, stdout, stderr io.Writer) error {
+	fs := pflag.NewFlagSet("commit", pflag.ContinueOnError)
+	fs.SetOutput(stderr)
+	timeout := fs.Duration("timeout", 10*time.Second, "Operation timeout")
+	verbose := fs.BoolP("verbose", "v", false, "Debug logging to stderr")
+	if err := fs.Parse(args); err != nil {
+		return &ExitError{Code: 1, Err: err}
+	}
+	if fs.NArg() != 1 {
+		return &ExitError{Code: 1, Err: fmt.Errorf("commit: expected exactly one MAC argument")}
+	}
+	mac, err := normalizeMAC(fs.Arg(0))
+	if err != nil {
+		return &ExitError{Code: 1, Err: err}
+	}
+
+	client, err := newClient(*verbose)
+	if err != nil {
+		return &ExitError{Code: 2, Err: err}
+	}
+	defer client.Close()
+
+	device, err := discoverAndFind(client, mac, *timeout)
+	if err != nil {
+		return err
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), *timeout)
+	defer cancel()
+	if err := client.SaveDeviceConfigWithContext(ctx, device); err != nil {
+		return &ExitError{Code: 2, Err: fmt.Errorf("commit (save) failed: %w", err)}
+	}
+	if err := client.ResetDeviceWithContext(ctx, device); err != nil {
+		return &ExitError{Code: 2, Err: fmt.Errorf("commit (reset) failed: %w", err)}
+	}
+	return nil
+}
---
+++ cli/reset.go
@@ -0,0 +1,44 @@
+package cli
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"time"
+
+	"github.com/spf13/pflag"
+)
+
+func runReset(args []string, stdout, stderr io.Writer) error {
+	fs := pflag.NewFlagSet("reset", pflag.ContinueOnError)
+	fs.SetOutput(stderr)
+	timeout := fs.Duration("timeout", 5*time.Second, "Operation timeout")
+	verbose := fs.BoolP("verbose", "v", false, "Debug logging to stderr")
+	if err := fs.Parse(args); err != nil {
+		return &ExitError{Code: 1, Err: err}
+	}
+	if fs.NArg() != 1 {
+		return &ExitError{Code: 1, Err: fmt.Errorf("reset: expected exactly one MAC argument")}
+	}
+	mac, err := normalizeMAC(fs.Arg(0))
+	if err != nil {
+		return &ExitError{Code: 1, Err: err}
+	}
+
+	client, err := newClient(*verbose)
+	if err != nil {
+		return &ExitError{Code: 2, Err: err}
+	}
+	defer client.Close()
+
+	device, err := discoverAndFind(client, mac, *timeout)
+	if err != nil {
+		return err
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), *timeout)
+	defer cancel()
+	if err := client.ResetDeviceWithContext(ctx, device); err != nil {
+		return &ExitError{Code: 2, Err: fmt.Errorf("reset failed: %w", err)}
+	}
+	return nil
+}
