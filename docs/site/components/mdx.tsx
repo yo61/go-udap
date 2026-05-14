44bb86ffb2d1697000e6084737bfa51eb3ad162b
Robin Bowes <robin@yo61.com>
2026-05-14 01:22:44 +0100

    build(docs): add <HowTo> MDX structural component

--- docs/site/components/mdx.tsx
+++ docs/site/components/mdx.tsx
@@ -1,9 +1,11 @@
 import defaultMdxComponents from 'fumadocs-ui/mdx';
 import type { MDXComponents } from 'mdx/types';
+import { HowTo } from './how-to';
 
 export function getMDXComponents(components?: MDXComponents) {
   return {
     ...defaultMdxComponents,
+    HowTo,
     ...components,
   } satisfies MDXComponents;
 }
